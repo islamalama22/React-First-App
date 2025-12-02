2a921d1e3049390a115f64f6f939b1900fb303db
islamalama22 <islamalama22@gmail.com>
2025-12-02 18:32:10 +0200

    spa  of session 3

--- reactApp/src/App.jsx
+++ reactApp/src/App.jsx
@@ -1,26 +1,29 @@
 import { useState } from "react";
 import Navbar from "./assets/components/navbar/Navbar.jsx";
 import Users from "./assets/components/users/users.jsx";
-
+import Services from "./assets/components/services/Services.jsx";
+import Contact from "./assets/components/contact/Contact.jsx";
+import Home from "./assets/components/home/Home.jsx";
+import about from "./assets/components/about/About.jsx";
+import Footer from "./assets/components/footer/Footer.jsx";
 function App() {
-  const [users, setUsers] = useState([
-    { "id": 1, "name": "islam", "email": "islamalam22@gmail.com" },
-    { "id": 2, "name": "alama", "email": "imalam22@gmail.com" },
-    { "id": 3, "name": "alla", "email": "joodmalam22@gmail.com" }
-  ]);
+  return (
+    <>
+      <Navbar/>
+      <Routes>
 
-  return <>
-    <section className="users">
-      <div className="container">
-        <div className="row">
+     <Route path ="/" element={<Home/>}></Route>
+     <Route path ="/contact" element={<Contact/>}></Route>
+     <Route path ="/about" element={<About/>}></Route>
+     <Route path ="/services" element={<Services/>}></Route>
+     <Route path ="/" element={<Home/>}></Route>
 
-          {users.map((user) =>
-            <Users id={user.id} name={user.name} email={user.email} />
-          )}
-        </div>
-      </div>
-    </section>
-  </>
+
+
+      </Routes>
+      <Footer/>
+    </>
+  )
 }
 
 
